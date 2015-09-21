110cda3fbed23e0c6c04c461c52e560be03f7f74
jlnguy <jnguy2014@my.fit.edu>
2015-09-21 10:27:09 -0400

    Update MultiplePerson.java
    Some loop to add more than one person into the elevator, while keeping the passenger number underneath the capacity.

--- MultiplePerson.java
+++ MultiplePerson.java
@@ -1,1 +1,48 @@
+package project;
 
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class MultiplePerson extends Elevator {
+
+    public MultiplePerson(int capacity, int timeMoveOneFloor,
+                          int floors, int doorDelta, boolean verbose) {
+        super(capacity, timeMoveOneFloor, floors, doorDelta, verbose);
+    }
+
+    public boolean continueOperate() {
+        return true;
+    }
+
+    public ArrayList<PassengerReleased> move() {
+        ArrayList<PassengerReleased> released =
+                new ArrayList<PassengerReleased>();
+        return released;
+    }
+
+    public void initialize(Queue<PassengerRequest> requests) {
+        servingQueue = requests;
+    }
+
+    public static void main(final String[] args) {
+        Random rnd = new Random(0);
+        Time startingTime = new Time(8,0,0);   // 8 am
+
+        int maxWeight = 250;
+        int capacity = maxWeight * 4;
+        int floors = 9;
+        int secondsPerFloor = 10; // Between each floor, there is a 10 second wait.
+        int timeOpenDoors = 15; // 15 seconds to open the door
+        long currentTime = startingTime.getTime();
+        int maxSeconds = 200;
+        boolean verbose = true;
+        int numberOfPassangers = 15;  		   // number of passengers to generate
+        Queue<PassengerRequest> elevatorQueue = new LinkedList<PassengerRequest>();
+
+        int tempPassenger = numberOfPassangers;
+        
+    }
+}
